fcbb61182e1cc352dcff53701160b7c240208073
Newton Bii <biinewton382@gmail.com>
2018-01-18 12:25:17 +0300

    Add landing page for the projects.

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=30)
+    web_link  = models.CharField(max_length=50, null=True)
+    repo_link = models.CharField(max_length=50, null=True)
     description = models.TextField()
-    article_image = models.ImageField(upload_to = 'articles/', null = True, blank = True)
-    web_link  = models.CharField(max_length=50)
+    project_image = models.ImageField(upload_to = 'projects/', null=True)
+
     def save_projects(self):
         self.save()
--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
 def all_projects(request):
-    return render(request, 'projects.html')
+    projects = Project.objects.all()
+    title = 'My Projects'
+    for project in projects:
+        print(project.title)
+        print(project.description)
+    return render(request, 'projects.html', {"projects":projects, "title":title})
