b307b08f79c7bb43d1d881c35174fc0f7552d0bc
Newton Bii <biinewton382@gmail.com>
2018-01-18 09:53:10 +0300

    Write tests for saving projects.

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -5,3 +5,5 @@ class Project(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     # article_image = models.ImageField(upload_to = 'articles/', null = True, blank = True)
+    def save_projects(self):
+        self.save()
--- portfolio/projects/tests.py
+++ portfolio/projects/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Project
 
 # Create your tests here.
+class ProjectTestClass(TestCase):
+
+    def setUp(self):
+        self.project = Project(title='twitfluence', description='It calculates the twitter score of users')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_projects(self):
+        self.project.save_projects()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)>0)
+
+    def tearDown(self):
+        self.project.save_projects()
+        Project.objects.all().delete()
