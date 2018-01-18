843e2c06b2a7c580715aabd48aa7d4cea1246021
Newton Bii <biinewton382@gmail.com>
2018-01-18 11:11:29 +0300

    add coulum for links.

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -5,5 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     article_image = models.ImageField(upload_to = 'articles/', null = True, blank = True)
+    web_link  = models.CharField(max_length=50)
     def save_projects(self):
         self.save()
