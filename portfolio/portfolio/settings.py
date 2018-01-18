1d02aef5efff9c4939c7d77fa7c23da6f1d6ac72
Newton Bii <biinewton382@gmail.com>
2018-01-18 09:39:25 +0300

    Create new projects models.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'newton',
+        'PASSWORD': 'newton'
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    # article_image = models.ImageField(upload_to = 'articles/', null = True, blank = True)
