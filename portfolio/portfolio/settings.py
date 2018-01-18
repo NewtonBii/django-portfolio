fc790c95eb65677a405be7455bbec3ae0276c339
Newton Bii <biinewton382@gmail.com>
2018-01-18 14:57:42 +0300

    Configure for deployment.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStrorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
