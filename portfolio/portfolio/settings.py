917b72fd95bd2fb69281500896693f62d3ade9a3
Newton Bii <biinewton382@gmail.com>
2018-01-18 14:45:26 +0300

    add files to deploy.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -125,3 +125,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStrorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
